3b1cda9cf6de2ce16dac8b9dda69e4f235ab1306
tsubo11 <tsubosaki.h11@gmail.com>
2026-03-03 06:52:59 +0900

    ログイン・新規登録画面作成/ログアウト導線設置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+
+    def after_sign_in_path_for(resource)
+        root_path
+    end
+
+    def after_sign_out_path_for(resource_or_scope)
+        root_path
+    end
 end
