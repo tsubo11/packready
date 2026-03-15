dae1afa6901aa9f045fbf4e9bdb6ca38b1b77a17
tsubo11 <tsubosaki.h11@gmail.com>
2026-03-15 20:11:13 +0900

    モーダル開閉を実装

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("hello", HelloController)
 
 import DropdownController from "./dropdown_controller"
 application.register("dropdown", DropdownController)
+
+import ModalController from "./modal_controller"
+application.register("modal", ModalController)
