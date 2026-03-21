6b810ba5e1e28817dccf34b634bf1ea279f3f387
tsubo11 <tsubosaki.h11@gmail.com>
2026-03-21 17:51:29 +0900

    リスト一覧・詳細画面で進捗表示

--- app/models/packing_list.rb
+++ app/models/packing_list.rb
@@ -3,4 +3,14 @@ class PackingList < ApplicationRecord
   
   belongs_to :user
   has_many :items, dependent: :destroy
+
+  def morning_progress
+    morning_items = items.where(timing: :morning)
+    { checked: morning_items.where(checked: true).count, total: morning_items.count }
+  end
+
+  def day_before_progress
+    day_before_items = items.where(timing: :day_before)
+    { checked: day_before_items.where(checked: true).count, total: day_before_items.count }
+  end
 end
