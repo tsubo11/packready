7b8c03d7d4654234f5a8bf962b08dd9a13dbadec
tsubo11 <tsubosaki.h11@gmail.com>
2026-04-22 07:33:31 +0900

    コメント追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,13 @@
 class Item < ApplicationRecord
+  # アソシエーション
+  # ItemがPackingListに属している
   belongs_to :packing_list
 
-  enum timing: { morning: 0, day_before: 1 }
-
+  # バリデーション
+  # 入力必須とする
   validates :name, presence: true
   validates :timing, presence: true
+
+  # 各タイミングを表す整数に対して、名前を割り当てる
+  enum timing: { morning: 0, day_before: 1 }
 end
