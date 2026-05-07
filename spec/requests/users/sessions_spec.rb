8a4c27b588ddcc38c626c0cb674667e95f593875
tsubo11 <tsubosaki.h11@gmail.com>
2026-05-07 23:54:01 +0900

    fix:context修正

--- spec/requests/users/sessions_spec.rb
+++ spec/requests/users/sessions_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe "Users::Sessions", type: :request do
   let(:user) { create(:user) }
 
   describe 'POST /users/sign_in' do
-    context 'ログイン成功' do
+    context 'ログイン成功した場合' do
       it "リスト一覧に遷移" do
         post "/users/sign_in", params: { user: { email: user.email, password: "password" } }
         expect(response).to redirect_to(packing_lists_path)
       end
     end
 
-    context 'ログイン失敗' do
+    context 'ログイン失敗した場合' do
       it "エラーメッセージが表示される" do
         post "/users/sign_in", params: { user: { email: user.email, password: "wrongpassword" } }
         expect(response).to have_http_status(422)
@@ -21,7 +21,7 @@ RSpec.describe "Users::Sessions", type: :request do
   end
 
   describe 'DELETE /users/sign_out' do
-    context 'ログアウト成功' do
+    context 'ログアウト成功した場合' do
       it 'トップ画面に遷移' do
         # ログインしてから
         post "/users/sign_in", params: { user: { email: user.email, password: "password" } }
