a34d12713907f8825d1c1be4b5266249eb131c1a
tsubo11 <tsubosaki.h11@gmail.com>
2026-05-10 01:07:58 +0900

    コメント追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
+    # 未ログインの場合、ログイン画面に遷移
     before_action :authenticate_user!
 
+    # ログイン後リスト一覧画面に遷移
     def after_sign_in_path_for(resource)
         packing_lists_path
     end
 
+    # ログアウト後トップ画面に遷移
     def after_sign_out_path_for(resource_or_scope)
         root_path
     end
