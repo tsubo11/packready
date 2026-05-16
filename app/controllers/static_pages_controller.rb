887f75d92ddedc077bc91aaccee74083ef1ffb93
tsubo11 <tsubosaki.h11@gmail.com>
2026-05-16 19:49:46 +0900

    Rubocopによる修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[ top terms privacy ]
+  skip_before_action :authenticate_user!, only: %i[top terms privacy]
   
   def top; end
   def terms; end
