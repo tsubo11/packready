08643acc13bc238f7639e7bb78c8661ed433c94f
tsubo11 <tsubosaki.h11@gmail.com>
2026-05-17 19:31:54 +0900

    add:トップ画面に使い方等追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
+  # 未ログインでもアクセス可能にする
   skip_before_action :authenticate_user!, only: %i[top terms privacy]
   
-  def top; end
+  # ログイン済みばらリスト一覧へ遷移
+  def top
+    redirect_to packing_lists_path if user_signed_in?
+  end
+
   def terms; end
   def privacy; end
 end
